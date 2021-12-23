0ccb678d19917b15d4fe4e900517ed353969c897
nzaih1999 <nzaih18@gmail.com>
2021-12-23 12:47:09 +0300

    updates

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,6 +1,10 @@
 import React from 'react'
-
+import { useContext } from 'react'
+import { RoomContext } from '../context'
+import Title from "../components/Title"
 export default function RoomFilter() {
+    const context = useContext(RoomContext)
+    console.log(context)
     return (
         <div>
            hello from room filter 
