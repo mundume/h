931c0922c6a41561a8bab2f07b937b6dd5824101
nzaih1999 <nzaih18@gmail.com>
2021-12-23 13:22:11 +0300

    updates

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -4,10 +4,27 @@ import { RoomContext } from '../context'
 import Title from "../components/Title"
 export default function RoomFilter() {
     const context = useContext(RoomContext)
-    console.log(context)
+    const {
+        handleChange,type,capacity,price,minPrice,maxPrice,minSize,maxSize,breakfast,pets
+    }=context
     return (
-        <div>
-           hello from room filter 
-        </div>
+        <section className='filter-container'>
+           <Title title='Search Rooms'/>
+           <form className='filter-form'>
+               
+               <div className='form-group'>
+                   <label htmlFor='type' className='form-group'>room type</label>
+                   <select
+                    name='type' 
+                    id='type' 
+                    value={type} 
+                    className='form-control'
+                     onChange={handleChange}/>
+
+                </div>
+                
+           </form>
+        </section>
+
     )
 }
