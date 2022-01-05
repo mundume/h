bd7ea45e274a686dd0d6108c5e9082eb3479ce9a
nzaih1999 <nzaih18@gmail.com>
2022-01-05 17:42:36 +0300

    shida tupu

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -2,11 +2,34 @@ import React from 'react'
 import { useContext } from 'react'
 import { RoomContext } from '../context'
 import Title from "../components/Title"
+import { render } from '@testing-library/react'
+//get All unique values
+const getUnique= (items,value)=>{
+    return [...new Set(items.map(item => item[value]))];
+};
 export default function RoomFilter(rooms) {
     const context = useContext(RoomContext)
     const {
-        handleChange,type,capacity,price,minPrice,maxPrice,minSize,maxSize,breakfast,pets
-    }=context
+        handleChange,
+        type,
+        capacity,
+        price,
+        minPrice,
+        maxPrice,
+        minSize,
+        maxSize,
+        breakfast,
+        pets
+         }=context
+         //get unique types
+    let types = getUnique(rooms, 'type');
+    //add all
+    types = ['all',...types]
+    //map to jsx
+    types = types.map((item,index)=>{
+        return<option value={item} key={index}>{item}</option>
+    })
+
     return (
         <section className='filter-container'>
            <Title title='Search Rooms'/>
@@ -20,8 +43,8 @@ export default function RoomFilter(rooms) {
                     value={type} 
                     className="form-control"
                      onChange={handleChange}> 
-                     
-                     
+                      {types}
+                    
                      </select>
 
                 </div>
